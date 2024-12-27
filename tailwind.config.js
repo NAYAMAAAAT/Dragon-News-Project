e72a5e22fd88b73c295443d7f984c87aaa450a7d
NAYAMAAAAT <atefmdnayamatullah@gmail.com>
2024-12-27 12:02:18 +0600

    design part done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "serif"],
+      },
+    },
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
+};
